284f9d5966ef711ce33b9ebf4ff82c2eca6838dd
stefanutti <mario.stefanutti@gmail.com>
2017-06-06 20:06:11 +0000

    sets

---
+++ set-no-proxy.sh
@@ -0,0 +1,3 @@
+export HTTP_PROXY=
+export HTTPS_PROXY=
+export ALL_PROXY=
---
+++ set-proxy-xxx.sh
@@ -0,0 +1,3 @@
+export HTTP_PROXY=my_proxy:port
+export HTTPS_PROXY=my_proxy:port
+export ALL_PROXY=socks://my_proxy:port/
