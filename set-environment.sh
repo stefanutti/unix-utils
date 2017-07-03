c2433c972ac5b962c7f297e19e7ad597dc61d5e4
mario.stefanutti@gmail.com <mario.stefanutti@gmail.com>
2017-07-03 22:35:24 +0100

    Some changes

--- set-environment.sh
+++ set-environment.sh
@@ -1,6 +1,7 @@
-export PRJ_HOME="$HOME/stefanutti/prj"
-
-alias goto=". $PRJ_HOME/unix-utils/scripts/goto.sh"
+export PRJ_HOME="$HOME/gv"
 
 export PATH=.:$UNIX_UTILS_HOME/unix-utils/tools/jmeter/bin:$PATH
+
+alias goto=". $PRJ_HOME/unix-utils/scripts/goto.sh"
+alias wizard="perl $PRJ_HOME/unix-utils/scripts/wizard.pl"
 
